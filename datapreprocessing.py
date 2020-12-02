8c3711eb08de4a6e462a716c7667e5c0614cf4c3
huguesrag <74901324+huguesrag@users.noreply.github.com>
2020-12-02 15:51:29 +0100

    Update dataprocessing.py
    Compute cyclical features

--- datapreprocessing.py
+++ datapreprocessing.py
@@ -5,14 +5,17 @@ Created on Tue Dec  1 19:51:14 2020
 @author: miguens1u
 
 Inspired by : https://towardsdatascience.com/all-about-categorical-variable-encoding-305f3361fd02
+and : https://towardsdatascience.com/cyclical-features-encoding-its-about-time-ce23581845ca
 """
 import pandas as pd
+import numpy as np
 
 def preprecessing(file = "data/Radar_Traffic_Counts.csv"):
     """preprocessing main function"""
     data = pd.read_csv(file, header = 0, nrows = 1000)
     data = encodeDirection(data)
     data = encodeTime(data)
+    data = aggregateVolume(data)
     return data
 
 def encodeDirection(data):
@@ -29,6 +32,35 @@ def encodeTime(data):
     data["Month"] = data.Month.astype('float')
     data["Day"] = data.Day.astype('float')
     data["Day of Week"] = data["Day of Week"].astype('float')
+    data["Hour"] = data["Hour"].astype('float')
+    data["Minute"] = data["Minute"].astype('float')
+    
+    data["Time"] = 60*data["Hour"]+data["Minute"]
+    
+    time_spec = {"Month" : 12., "Day" : 30., "Day of Week" : 7., "Time" : 1440.}
+    time_feature = list(time_spec.keys())
+    for feature in time_feature:
+        data[feature+"_norm"] = 2*np.pi*data[feature]/time_spec[feature]
+        data["cos_"+feature] = np.cos(data[feature+"_norm"])
+        data["sin_"+feature] = np.sin(data[feature+"_norm"])
+        data.drop(feature+"_norm", axis=1, inplace=True)
+    
+    return data
+
+def aggregateVolume(data):
+    """Sum the volumes of rows describing the same event"""
+    
+    data["concat"] = (data["location_name"].astype('str') + data["Year"].astype('str') 
+                      + data["Month"].astype('str') + data["Day"].astype('str')
+                      + data["Day of Week"].astype('str') + data["Time Bin"].astype('str')
+                      + data["direction_x"].astype('str') + data["direction_y"].astype('str'))
+    aggdata = data.groupby("concat")["concat", "Volume"].sum()
+    
+    data = data.drop_duplicates(subset="concat")
+    data.drop(["Volume"], axis=1, inplace=True)
+    data = pd.merge(data, aggdata, on="concat")
+    data.drop(["concat"], axis=1, inplace=True)
+    
     return data
 
 if __name__ == '__main__':
