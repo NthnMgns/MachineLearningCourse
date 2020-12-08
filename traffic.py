9e33b42e4b2c093a8cc9897da0546499029e44a6
NthnMgns <nathan.miguens@gmail.com>
2020-12-09 00:34:45 +0100

    Features encoding finish
    X, y for test and train well defined

--- traffic.py
+++ traffic.py
@@ -1,43 +1,32 @@
 # -*- coding: utf-8 -*-
 """
+Created on Wed Dec  9 00:29:09 2020
+
+@author: miguens1u
+"""
+
+# -*- coding: utf-8 -*-
+"""
 Created on Mon Nov 23 17:32:39 2020
 
 @author: Nathan Miguens & Hugues Raguenes
 
-Objectif : A définir
+Contexte : Une entreprise essaie d'estimer le trafic d'un carrefour. Elle 
+        enregistre pendant 2 ans ce trafic à l'aide d'un capteur. Par la suite,
+        ce capteur est nécessaire pour une autre utilisation. Il doit donc être
+        retirer du carrefour mais l'on souhaite toujours connaitre son trafic
+
+Objectif : Estimer le trafic de ce carrefour avec les données qu'on récolte 
+        ailleurs dans la ville.
+        
+        Précision : Trouver le trafic du lieu à un instant donné connaissant le trafic des
+        autres lieux à ce même moment.
+        
+Choix du lieu :  BURNET RD / PALM WAY (IBM DRIVEWAY)
+
+
+# 1er model : FCNNs
+# 2nd model : CNNs
+# 3rd model : LSTM
 """
 
-import torch
-import pandas as pd
-import numpy as np
-import pickle
-
-from torch.utils.data import DataLoader
-
-def loadData(path):
-    """Load data from pickle serialized file"""
-    return pd.DataFrame(pickle.load(open( path, "rb" )))
-
-def splitData(data, train_ratio = [0.6, 0.5], random_state = 0):
-    """Split data in train, test and valid sets"""
-    train_set = data.sample(frac= train_ratio[0], random_state = random_state)
-    test_set = data.drop(train_set.index)
-    
-    valid_set = test_set.sample(frac= train_ratio[1], random_state = random_state)
-    test_set = test_set.drop(valid_set.index)
-    
-    return pandasToTorch([train_set, test_set, valid_set])
-
-def pandasToTorch(dataFrames):
-    """Transform pandas data to torch data"""
-    tData = []
-    for dataFrame in dataFrames :
-        tData.append(DataLoader(dataFrame.values, batch_size=512))
-    return tData
-
-
-    
-if __name__ == '__main__':
-    data = loadData("data/preprocessedData.p")
-    train_set, test_set, valid_set = splitData(data)
-    
